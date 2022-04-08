92c7991f20d5682c1bfc080541ff470347fc72a4
Mateusz Pietrzak <mateusz@mzpkjs.com>
2022-04-08 19:25:15 +0200

    fix: wrap throwing functions in a function for testing purposes

--- exercise-01/exercise-01.spec.js
+++ exercise-01/exercise-01.spec.js
@@ -49,11 +49,11 @@ describe("function fizzbuzz()", () => {
     })
 
     it("should throw Error for an input which is not parsable to number", () => {
-        expect(fizzbuzz(null))
+        expect(() => fizzbuzz(null))
             .toThrow(new Error())
-        expect(fizzbuzz(undefined))
+        expect(() => fizzbuzz(undefined))
             .toThrow(new Error())
-        expect(fizzbuzz(Infinity))
+        expect(() => fizzbuzz(Infinity))
             .toThrow(new Error())
     })
 })
