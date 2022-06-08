d5a2a25ba0f4b35323ed9adc12726b23e796d5e8
Mateusz Pietrzak <mateusz@mzpkjs.com>
2022-06-08 12:37:16 +0200

    fix: add more tests

--- exercise-07/exercise-07.spec.js
+++ exercise-07/exercise-07.spec.js
@@ -3,8 +3,17 @@ const choose = require("./exercise-07")
 
 describe("function choose(choices)", () => {
     it("should return a value from `choices` argument chosen at random", () => {
-        const petto = choose("doggo", "catto", "ratto")
+        const petto = choose("doggo", "catto", "ratto", "snekk", "goose")
+
         expect(petto())
           .toHaveLength(5)
+        expect(["doggo", "catto", "ratto", "snekk", "goose"].includes(petto()))
+          .toBe(true)
+
+        // This can fail occasionally, just re-run it
+        const a = petto(),
+          b = petto()
+        expect(a)
+          .not.toEqual(b)
     })
 })
